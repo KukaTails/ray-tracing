a410102abb95914ac14a75b737a79e2d9e8d409d
KukaTails <heweidong526@gmail.com>
2016-04-25 23:25:11 +0800

    modify point and vector

--- src/geometry/point.h
+++ src/geometry/point.h
@@ -1,9 +1,145 @@
 #ifndef LEPTUS_POINT_H
 #define LEPTUS_POINT_H
 
-#include "leptus.h"
+#include "core/leptus.h"
 
 namespace leptus {
+
+template <typename T>
+class Point2 {
+public:
+  T x_, y_;
+
+public:
+  Point2(): x_(0), y_(0) {}
+  Point2(T x, T y) : x_(x), y_(y) { Assert(!HasNaNs()); }
+  explicit Point2(const Point3<T>& p) : x_(p.x_), y_(p.y_) { Assert(!HasNaNs()); }
+
+  template <typename U>
+  explicit Point2(const Point2<U>& p)
+  {
+    x_ = (T)p.x_;
+    y_ = (T)p.y_;
+    Assert(!HasNaNs());
+  }
+
+  template <typename U>
+  explicit Point2(const Vector2<U>& p)
+  {
+    x_ = (T)p.x_;
+    y_ = (T)p.y_;
+    Assert(!HasNaNs());
+  }
+
+  template <typename U>
+  explicit operator Vector2<U>() const
+  {
+    return Vector2<U>(x_, y_);
+  }
+
+  friend std::ostream& operator<<(std::ostream& os, const Point2<T>& p)
+  {
+    os << "[" << p.x_ << ", " << p.y_ << "]";
+    return os;
+  }
+
+  Point2<T> operator+(const Vector2<T>& v) const
+  {
+    Assert(!v.HasNaNs());
+    return Point2<T>(x_ + v.x_, y_ + v.y_);
+  }
+
+  Point2<T>& operator+=(const Vector2<T>& v)
+  {
+    Assert(!v.HasNaNs());
+    x_ += v.x_;
+    y_ += v.y_;
+    return *this;
+  }
+
+  Vector2<T> operator-(const Point2<T>& p) const
+  {
+    Assert(!p.HasNaNs());
+    return Vector2<T>(x_ - p.x_, y_ - p.y_);
+  }
+
+  Point2<T> operator-(const Vector2<T>& v) const
+  {
+    Assert(!v.HasNaNs());
+    return Point2<T>(x_ - v.x_, y_ - v.y_);
+  }
+
+  Point2<T> operator-() const { return Point2<T>(-x_, -y_); }
+  Point2<T>& operator-=(const Vector2<T>& v)
+  {
+    Assert(!v.HasNaNs());
+    x_ -= v.x_;
+    y_ -= v.y_;
+    return *this;
+  }
+
+  Point2<T>& operator+=(const Point2<T>& p)
+  {
+    Assert(!p.HasNaNs());
+    x_ += p.x_;
+    y_ += p.y_;
+    return *this;
+  }
+
+  Point2<T> operator+(const Point2<T>& p) const
+  {
+    Assert(!p.HasNaNs());
+    return Point2<T>(x_ + p.x_, y_ + p.y);
+  }
+
+  template <typename U>
+  Point2<T> operator*(U f) const
+  {
+    return Point2<T>(f * x_, f * y_);
+  }
+  template <typename U>
+  Point2<T>& operator*=(U f)
+  {
+    x_ *= f;
+    y_ *= f;
+    return *this;
+  }
+
+  template <typename U>
+  Point2<T> operator/(U f) const
+  {
+    Float inv = (Float)1 / f;
+    return Point2<T>(inv * x_, inv * y_);
+  }
+
+  template <typename U>
+  Point2<T>& operator/=(U f)
+  {
+    Float inv = (Float)1 / f;
+    x_ *= inv;
+    y_ *= inv;
+    return *this;
+  }
+
+  T operator[](int i) const
+  {
+    Assert(i >= 0 && i <= 1);
+    if (i == 0) return x_;
+    return y_;
+  }
+
+  T& operator[](int i)
+  {
+    Assert(i >= 0 && i <= 1);
+    if (i == 0) return x_;
+    return y_;
+  }
+
+  bool operator==(const Point2<T>& p) const { return x_ == p.x_ && y_ == p.y; }
+  bool operator!=(const Point2<T>& p) const { return x_ != p.x_ || y_ != p.y; }
+  bool HasNaNs() const { return IsNaN(x_) || IsNaN(y_); }
+};
+
 
 template <typename T>
 class Point3 {
@@ -142,7 +278,8 @@ public:
   bool HasNaNs() const { return IsNaN(x_) || IsNaN(y_) || IsNaN(z_); }
 };
 
-
+typedef Point2<int> Point2i;
+typedef Point2<Float> Point2f;
 typedef Point3<int> Point3i;
 typedef Point3<Float> Point3f;
 
--- src/geometry/vec.h
+++ src/geometry/vec.h
@@ -1,9 +1,120 @@
 #ifndef LEPTUS_VEC_H
 #define LEPTUS_VEC_H
 
-#include "leptus.h"
+#include "core/leptus.h"
 
 namespace leptus {
+
+template <typename T>
+class Vector2 {
+public:
+  T x_, y_;
+
+public:
+  Vector2():x_(0), y_(0) { }
+  Vector2(T x, T y) : x_(x), y_(y) { Assert(!HasNaNs()); }
+  bool HasNaNs() const { return IsNaN(x_) || IsNaN(y_); }
+  explicit Vector2(const Point2<T>& p);
+  explicit Vector2(const Point3<T>& p);
+
+  friend std::ostream& operator<<(std::ostream& os, const Vector2<T>& v)
+  {
+    os << "[" << v.x_ << ", " << v.y_ << "]";
+    return os;
+  }
+
+  Vector2<T> operator+(const Vector2<T>& v) const
+  {
+    Assert(!v.HasNaNs());
+    return Vector2(x_ + v.x_, y_ + v.y_);
+  }
+
+  Vector2<T>& operator+=(const Vector2<T>& v)
+  {
+    Assert(!v.HasNaNs());
+    x_ += v.x_;
+    y_ += v.y_;
+    return *this;
+  }
+  Vector2<T> operator-(const Vector2<T>& v) const
+  {
+    Assert(!v.HasNaNs());
+    return Vector2(x_ - v.x, y_ - v.y_);
+  }
+
+  Vector2<T>& operator-=(const Vector2<T>& v)
+  {
+    Assert(!v.HasNaNs());
+    x_ -= v.x_;
+    y_ -= v.y_;
+    return *this;
+  }
+
+  bool operator==(const Vector2<T>& v) const { return x_ == v.x_ && y_ == v.y_; }
+  bool operator!=(const Vector2<T>& v) const { return x_ != v.x_ || y_ != v.y_; }
+
+  template <typename U>
+  Vector2<T> operator*(U f) const
+  {
+    return Vector2<T>(f * x_, f * y_);
+  }
+
+  template <typename U>
+  Vector2<T>& operator*=(U f)
+  {
+    Assert(!IsNaN(f));
+    x_ *= f;
+    y_ *= f;
+    return *this;
+  }
+
+  template <typename U>
+  Vector2<T> operator/(U f) const
+  {
+    Assert(f != 0);
+    Float inv = (Float)1 / f;
+    return Vector2<T>(x_ * inv, y_ * inv);
+  }
+
+  template <typename U>
+  Vector2<T>& operator/=(U f)
+  {
+    Assert(f != 0);
+    Float inv = (Float)1 / f;
+    x_ *= inv;
+    y_ *= inv;
+    return *this;
+  }
+
+  Vector2<T> operator-() const { return Vector2<T>(-x_, -y_); }
+
+  T operator[](int i) const
+  {
+    Assert(i >= 0 && i <= 1);
+    if (i == 0) return x_;
+    return y_;
+  }
+
+  T& operator[](int i)
+  {
+    Assert(i >= 0 && i <= 1);
+    if (i == 0) return x_;
+    return y_;
+  }
+
+  Float LengthSquared() const { return x_ * x + y_ * y; }
+  Float Length() const { return std::sqrt(LengthSquared()); }
+};
+
+template <typename T>
+Vector2<T>::Vector2(const Point2<T>& p)
+  :x_(p.x_), y_(p.y_)
+{}
+
+template <typename T>
+Vector2<T>::Vector2(const Point3<T>& p)
+  : x_(p.x_), y_(p.y_)
+{}
 
 template <typename T>
 class Vector3 {
@@ -12,6 +123,7 @@ public:
 
 public:
   Vector3() { x_ = y_ = z_ = T(0); }
+  Vector3(T x) { x_ = y_ = z_ = x; }
   Vector3(T x, T y, T z) : x_(x), y_(y), z_(z) { Assert(!HasNaNs()); }
   explicit Vector3(const Point3<T>& p);
   explicit Vector3(const Normal3<T>& n);
@@ -27,14 +139,14 @@ public:
   Vector3<T> operator+(const Vector3<T>& v) const
   {
     Assert(!v.HasNaNs());
-    return Vector3(x_ + v.x_, y_ + v.y_, z_ + v.z_);
+    return Vector3(x_ + v.x_, y_ + v.y__, z_ + v.z_);
   }
 
   Vector3<T>& operator+=(const Vector3<T>& v)
   {
     Assert(!v.HasNaNs());
     x_ += v.x_;
-    y_ += v.y_;
+    y_ += v.y__;
     z_ += v.z_;
     return *this;
   }
@@ -42,26 +154,26 @@ public:
   Vector3<T> operator-(const Vector3<T>& v)
   {
     Assert(!v.HasNaNs());
-    return Vector3(x - v.x_, y_ - v.y_, z_ - v.z_);
+    return Vector3(x - v.x_, y_ - v.y__, z_ - v.z_);
   }
 
   Vector3<T>& operator-=(const Vector3<T>& v)
   {
     Assert(!v.HasNaNs());
     x_ -= v.x_;
-    y_ -= v.y_;
+    y_ -= v.y__;
     z_ -= v.z_;
     return *this;
   }
 
   bool operator==(const Vector3<T>& v) const
   {
-    return x_ == v.x_ && y_ == v.y_ && z_ == v.z_;
+    return x_ == v.x_ && y_ == v.y__ && z_ == v.z_;
   }
 
   bool operator!=(const Vector3<T>& v) const
   {
-    return x_ != v.x_ || y_ != v.y_ || z_ == v.z_;
+    return x_ != v.x_ || y_ != v.y__ || z_ == v.z_;
   }
 
   template <typename U>
@@ -121,6 +233,9 @@ public:
   Float Length() const { return std::sqrt(LengthSquared()); }
 };
 
+
+typedef Vector2<Float> Vector2f;
+typedef Vector2<int> Vector2i;
 typedef Vector3<Float> Vector3f;
 typedef Vector3<int> Vector3i;
 
