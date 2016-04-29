d533e9176981f1baab1e44efa46e68dd2c5474a5
KukaTails <heweidong526@gmail.com>
2016-04-29 23:31:52 +0800

    add ambient light

---
+++ src/lights/ambient.cc
@@ -0,0 +1,37 @@
+#include "lights/ambient.h"
+
+namespace leptus {
+
+Ambient::Ambient()
+  : color_(WHITE), light_scale_(static_cast<Float>(1.0))
+{}
+
+Ambient::Ambient(Float light_scale, Color color /* = WHITE */)
+  : color_(color), light_scale_(light_scale)
+{}
+
+Ambient::Ambient(Color color, Float light_scale /* = static_cast<Float>(1.0) */)
+  : color_(color), light_scale_(light_scale)
+{}
+
+Color Ambient::light() const
+{
+  return light_scale_ * color_;
+}
+
+bool Ambient::CastShadows() const
+{
+  return false;
+}
+
+bool Ambient::InShadow(const Point3f& point, const std::vector<SurPtr>& objects) const
+{
+  return false;
+}
+
+Vector3f Ambient::GetShadowRayDir(const ShadeRecord& shade_record) const
+{
+  return Vector3f(static_cast<Float>(0));
+}
+
+} // namespace leptus
---
+++ src/lights/ambient.h
@@ -0,0 +1,27 @@
+#ifndef LEPTUS_AMBIENT_H
+#define LEPTUS_AMBIENT_H
+
+#include "core/light.h"
+#include "core/ray.h"
+#include "geometry/vec.h"
+
+namespace leptus {
+
+class Ambient : public Light {
+private:
+  Color color_;
+  Float light_scale_;
+
+public:
+  Ambient();
+  Ambient(Color color, Float light_scale = static_cast<Float>(1.0));
+  Ambient(Float light_scale, Color color = WHITE);
+  virtual Vector3f GetShadowRayDir(const ShadeRecord& shade_record) const override;
+  virtual Color light() const override;
+  virtual bool CastShadows() const override;
+  virtual bool InShadow(const Point3f& point, const std::vector<SurPtr>& objects) const override;
+};
+
+} // namespace leptus
+
+#endif // LEPTUS_AMBIENT_H
