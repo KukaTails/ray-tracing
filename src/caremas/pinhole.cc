4379645b21b68d2d182779abe133862dc6e93877
KukaTails <heweidong526@gmail.com>
2016-05-01 23:49:59 +0800

    add pinhole camera

---
+++ src/caremas/pinhole.cc
@@ -0,0 +1,21 @@
+#include "caremas/pinhole.h"
+
+namespace leptus {
+
+PinHoleCamera::PinHoleCamera(Float d)
+  :d_(d), Camera()
+{}
+
+PinHoleCamera::PinHoleCamera(Float d,
+                             const Point3f& eye,
+                             const Point3f& lookat,
+                             const Vector3f& up)
+  :d_(d), Camera(eye, lookat, up)
+{}
+
+Ray PinHoleCamera::GenerateRay(const CameraSample& sample) const
+{
+  return Ray();
+}
+
+} // namespace leptus
---
+++ src/caremas/pinhole.h
@@ -0,0 +1,29 @@
+#ifndef LEPTUS_PINHOLE_H
+#define LEPTUS_PINHOLE_H
+
+#include "core/camera.h"
+#include "core/leptus.h"
+#include "core/ray.h"
+#include "geometry/point.h"
+#include "geometry/vec.h"
+
+
+namespace leptus {
+
+class PinHoleCamera : public Camera {
+private:
+  Float d_;
+  Float zoom_;
+
+public:
+  PinHoleCamera(Float d = 1);
+  PinHoleCamera(Float d,
+                const Point3f& eye,
+                const Point3f& lookat,
+                const Vector3f& up = Vector3f(0.0, 1.1, 0.0));
+  virtual Ray GenerateRay(const CameraSample& sample) const override;
+};
+
+}  // namespace leptus
+
+#endif // LEPTUS_PINTHOLE_H
