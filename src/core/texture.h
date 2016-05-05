92bb31381ec236fa4480637e2700426efc8ef4e5
KukaTails <heweidong526@gmail.com>
2016-05-05 23:35:45 +0800

    add texture

---
+++ src/core/texture.h
@@ -0,0 +1,23 @@
+#ifndef LEPTUS_TEXTURE_H
+#define LEPTUS_TEXTURE_H
+
+#include <memory>
+
+#include "core/color.h"
+#include "core/hitrecord.h"
+
+namespace leptus {
+
+class Texture {
+public:
+  typedef std::shared_ptr<Texture> TexturePtr;
+public:
+  virtual ~Texture( ) = default;
+  virtual Color GetColor(const HitRecord& hit_rec) const = 0;
+};
+
+typedef Texture::TexturePtr TexturePtr;
+
+} // namespace leptus
+
+#endif // LEPTUS_TEXTURE_H
