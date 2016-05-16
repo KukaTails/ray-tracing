95ba1f39996e5f0fc7adce79c5388549db509605
TailsKuka <heweidong526@gmail.com>
2016-05-16 23:33:48 +0800

    add cylinder

---
+++ src/surfaces/cylinder.h
@@ -0,0 +1,17 @@
+#ifndef LEPTUS_CYLINDER_H
+#define LEPTUS_CYLINDER_H
+
+#include "core/surface.h"
+
+namespace leptus {
+
+class Cylinder : public Surface {
+public:
+  //Cylinder(const Point3f& center, Float radius, Float length, const Color& color);
+  //Cylinder(const Point3f& center, Float radiuse, Float length, const MaterialPtr& material);
+};
+
+
+} // namespace leptus
+
+#endif // LEPTUS_CYLINDER_H
