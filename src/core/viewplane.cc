e9d4312c32406085bbc1874838b8c1e9c2c304b8
KukaTails <heweidong526@gmail.com>
2016-05-25 21:18:37 +0800

    delete save method

--- src/core/viewplane.cc
+++ src/core/viewplane.cc
@@ -1,39 +1,17 @@
-#include <algorithm>
-#include <fstream>
-
 #include "core/viewplane.h"
 
 namespace leptus {
 
-ViewPlane::ViewPlane()
+ViewPlane::ViewPlane( )
   :h_res_(640), v_res_(480), pix_x_size_(1.0 / 640.0), pix_y_size_(1.0 / 480.0)
-{
-  image_ = std::make_shared<std::vector<Vector3f>>(v_res_ * h_res_);
-}
+{ }
 
 ViewPlane::ViewPlane(unsigned h_res, unsigned v_res)
   : h_res_(h_res), v_res_(v_res), pix_x_size_(1.0 / h_res), pix_y_size_(1.0 / v_res)
-{
-  image_ = std::make_shared<std::vector<Vector3f>>(v_res * h_res);
-}
+{ }
 
 ViewPlane::ViewPlane(unsigned h_res, unsigned v_res, Float pix_x_size, Float pix_y_size)
-  :h_res_(h_res), v_res_(v_res), pix_x_size_(pix_x_size), pix_y_size_()
-{
-  image_ = std::make_shared<std::vector<Vector3f>>(v_res * h_res);
-}
-
-void ViewPlane::Save(std::string file_path) const
-{
-  std::ofstream ofs(file_path, std::ios::out | std::ios::binary);
-
-  ofs << "P6\n" << h_res_ << " " << v_res_ << "\n255\n";
-  for (unsigned i = 0; i < h_res_ * v_res_; ++i) {
-    ofs << (unsigned char)(std::min(float(1), (*image_)[i].x_) * 255) <<
-      (unsigned char)(std::min(float(1), (*image_)[i].y_) * 255) <<
-      (unsigned char)(std::min(float(1), (*image_)[i].z_) * 255);
-  }
-  ofs.close();
-}
+  : h_res_(h_res), v_res_(v_res), pix_x_size_(pix_x_size), pix_y_size_( )
+{ }
 
 } // namespace leptus
--- src/core/viewplane.h
+++ src/core/viewplane.h
@@ -1,11 +1,6 @@
-#ifndef LEPTUS_VIEW_PLANE_H
-#define LEPTUS_VIEW_PLANE_H
-
-#include <memory>
-#include <vector>
-
+#ifndef LEPTUS_CORE_VIEW_PLANE_H
+#define LEPTUS_CORE_VIEW_PLANE_H
 #include "core/leptus.h"
-#include "geometry/geometry.h"
 
 namespace leptus {
 
@@ -13,15 +8,13 @@ class ViewPlane {
 public:
   unsigned h_res_, v_res_;
   Float pix_x_size_, pix_y_size_;
-  std::shared_ptr<std::vector<Vector3f>> image_;
 
 public:
   ViewPlane();
   ViewPlane(unsigned h_res, unsigned v_res);
   ViewPlane(unsigned h_res, unsigned v_res, Float pix_x_size, Float pix_y_size);
-  void Save(std::string file_path) const;
 };
 
 }  // namespace leptus
 
-#endif // LEPTUS_VIEW_PLANE_H
+#endif // LEPTUS_CORE_VIEW_PLANE_H
