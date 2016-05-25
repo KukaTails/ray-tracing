9ac6b96178f05183d33cf087f5203d1b611eaf04
KukaTails <heweidong526@gmail.com>
2016-05-25 21:20:28 +0800

    add pointer to world and tracer

--- src/core/shaderecord.cc
+++ src/core/shaderecord.cc
@@ -1,9 +1,23 @@
-#include "shaderecord.h"
+#include "core/shaderecord.h"
+
+#include "tracers/whitted.h"
 
 namespace leptus {
 
 ShadeRecord::ShadeRecord(const World& world)
-  : world_(world), is_hit_(false), color_(BLACK)
-{}
+  : world_(world), is_hit_(false), color_(BLACK), tracer_(WhittedTracer())
+{ }
+
+ShadeRecord::ShadeRecord(const ShadeRecord& shade_rec, const Tracer& tracer)
+  : tracer_(tracer), world_(shade_rec.world_)
+{
+  this->color_ = shade_rec.color_;
+  this->dir_ = shade_rec.dir_;
+  this->hit_rec_ = shade_rec.hit_rec_;
+  this->is_hit_ = shade_rec.is_hit_;
+  this->material_ = shade_rec.material_;
+  this->p_texture_ = shade_rec.p_texture_;
+  this->ray_ = shade_rec.ray_;
+}
 
 } // namespace leptus
--- src/core/shaderecord.h
+++ src/core/shaderecord.h
@@ -3,32 +3,30 @@
 #include <memory>
 
 #include "core/color.h"
+#include "core/hitrecord.h"
 #include "core/material.h"
 #include "core/ray.h"
-#include "geometry/normal.h"
-#include "geometry/point.h"
-#include "geometry/vec.h"
+#include "core/tracer.h"
+#include "core/world.h"
+#include "geometry/geometry.h"
 
 namespace leptus {
 
-class World;
-
 class ShadeRecord {
 public:
-  bool is_hit_;              // if the ray hit a surface
-  Point3f p_hit_;            // world coordinates of hit point
-  Normal3f n_hit_;           // normal at hit point
-
-  Point3f local_hit_point_;  // local coordinate system used to get texture
-  Ray ray_;                  // used to calculate specular highlights
-  Vector3f dir_;             // used to calculate area lights
-  int depth_;                 // recursion depth
-  Color color_;              // used to record the result color
+  bool is_hit_;                    // if the ray hit a surface
+  HitRecord hit_rec_;              // world coordinates of hit point and hit normal
+  Point2f p_texture_;              // local coordinate system used to get texture
+  Ray ray_;                        // used to calculate specular highlights
+  Vector3f dir_;                   // used to calculate area lights
+  Color color_;                    // used to record the result color
   MaterialPtr material_;     // nearest object's material
-  const World& world_;       // reference to world
+  const World& world_;           // pointer which point to world
+  const Tracer& tracer_;               // pointer which point to tracer
 
 public:
   ShadeRecord(const World& world);
+  ShadeRecord(const ShadeRecord& shade_rec, const Tracer& tracer);
 };
 
 } // namespace leptus
