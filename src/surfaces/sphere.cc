9977c9fec86d80438719309c1eda249daaeb27e0
KukaTails <heweidong526@gmail.com>
2016-05-25 21:03:04 +0800

    add shadow hit method

--- src/surfaces/sphere.cc
+++ src/surfaces/sphere.cc
@@ -1,6 +1,4 @@
-#include "core/ray.h"
 #include "surfaces/sphere.h"
-#include "geometry/geometry.h"
 
 namespace leptus {
 
@@ -12,22 +10,59 @@ Sphere::Sphere(const Point3f& center, Float radius, const MaterialPtr& material)
   : center_(center), radius_(radius), Surface(material)
 {}
 
-bool Sphere::Hit(const Ray& ray, Float& t_hit, ShadeRecord& shade_rec) const
+Point3f Sphere::GetLocalHitPoint(const Point3f& p_hit) const
+{
+  return Point3f((p_hit.x_ - center_.x_) / radius_,
+                 (p_hit.y_ - center_.y_) / radius_,
+                 (p_hit.z_ - center_.z_) / radius_);
+}
+
+bool Sphere::Hit(const Ray& ray, Float& t_hit, HitRecord& hit_rec) const
+{
+  /*
+  Vector3f l = center_ - ray.orig_;
+  Float tca = Dot(l, ray.dir_);
+  if (tca < 0) return false;
+
+  Float d2 = Dot(l, l) - tca * tca;
+  if (d2 > radius_ * radius_)
+    return false;
+  
+  Float thc = sqrt(radius_ * radius_- d2);
+  Float t0 = tca - thc, t1 = tca + thc;
+  if (t0 < 0)
+    t0 = t1;
+  if (t0 < 0)
+    return false;
+  t_hit = t0;
+  */
+  if (ShadowHit(ray, t_hit)) {
+    hit_rec.p_hit_ = ray.orig_ + ray.dir_ * t_hit;
+    hit_rec.n_hit_ = Normal3f(Normalize(hit_rec.p_hit_ - center_));
+    hit_rec.local_p_hit_ = GetLocalHitPoint(hit_rec.p_hit_);
+    return true;
+  }
+  return false;
+}
+
+bool Sphere::ShadowHit(const Ray& ray, Float& t_hit) const
 {
   Vector3f l = center_ - ray.orig_;
   Float tca = Dot(l, ray.dir_);
   if (tca < 0) return false;
 
   Float d2 = Dot(l, l) - tca * tca;
-  if (d2 > radius_ * radius_) return false;
-  
-  Float thc = sqrt(radius_ * radius_- d2);
+  if (d2 > radius_ * radius_)
+    return false;
+
+  Float thc = sqrt(radius_ * radius_ - d2);
   Float t0 = tca - thc, t1 = tca + thc;
   if (t0 < 0)
     t0 = t1;
+  if (t0 < 0)
+    return false;
   t_hit = t0;
-  shade_rec.p_hit_ = ray.orig_ + ray.dir_ * t0;
-  shade_rec.n_hit_ = Normal3f(Normalize(shade_rec.p_hit_ - center_));
+
   return true;
 }
 
--- src/surfaces/sphere.h
+++ src/surfaces/sphere.h
@@ -2,9 +2,10 @@
 #define LEPTUS_SPHERE_H
 
 #include "core/color.h"
+#include "core/hitrecord.h"
 #include "core/surface.h"
 #include "core/material.h"
-#include "geometry/point.h"
+#include "geometry/geometry.h"
 
 namespace leptus {
 
@@ -13,10 +14,13 @@ public:
   Float radius_;
   Point3f center_;
 
+private:
+  virtual Point3f GetLocalHitPoint(const Point3f& p_hit) const override;
 public:
   Sphere(const Point3f& center, Float radius, const Color& color);
   Sphere(const Point3f& center, Float radius, const MaterialPtr& material);
-  virtual bool Hit(const Ray& ray, Float& t_hit, ShadeRecord& shade_rec) const override;
+  virtual bool Hit(const Ray& ray, Float& t_hit, HitRecord& hit_rec) const override;
+  virtual bool ShadowHit(const Ray& ray, Float& t_hit) const override;
 };
 
 } // namespace leptus
