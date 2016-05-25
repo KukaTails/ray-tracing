c0230b34ca8a53936fbe07373e90207e348d93b0
KukaTails <heweidong526@gmail.com>
2016-05-25 20:59:24 +0800

    add image class to load pictures

--- src/core/image.h
+++ src/core/image.h
@@ -1,6 +1,8 @@
 #ifndef LEPTUS_IMAGE_H
 #define LEPTUS_IMAGE_H
+#include <string>
 #include <memory>
+
 #include "core/color.h"
 
 namespace leptus {
@@ -9,10 +11,21 @@ class Image {
 public:
   typedef std::shared_ptr<Image> ImagePtr;
 public:
-  Color GetColor(unsigned row, unsigned column) const
-  { 
-    return Color(0.0, 0.0, 0.0);
-  }
+  explicit Image(const std::string& image_path);
+  ~Image( );
+  Image(const Image&) = delete;
+  bool operator=(const Image&) = delete;
+  Color GetColor(float u, float v) const;
+  
+private:
+  void LoadImage( );
+  void Free( );
+
+private:
+  std::string image_path_;
+  unsigned char *data_;
+  int width_, height_;
+  bool success = false;
 };
 
 typedef Image::ImagePtr ImagePtr;
