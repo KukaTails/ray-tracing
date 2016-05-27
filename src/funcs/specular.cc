1ae6a2c3450c367e80c79d752a74df1fbf1dcd6c
KukaTails <heweidong526@gmail.com>
2016-05-27 23:48:27 +0800

    fix bug on shadering

--- src/funcs/specular.cc
+++ src/funcs/specular.cc
@@ -20,10 +20,18 @@ Color PerfectSpecular::F(const HitRecord& hit_rec, const Vector3f& in_dir, const
 
 Color PerfectSpecular::SampleF(const HitRecord& hit_rec, Vector3f& in_dir, const Vector3f& out_dir) const
 {
-  Float n_dot_out = Dot(hit_rec.n_hit_, out_dir);
-  in_dir = -out_dir + 2.0 * Vector3f(hit_rec.n_hit_) * n_dot_out;
+  Normal3f normal = hit_rec.n_hit_;
 
+  if (Dot(out_dir, normal) < 0.0)
+    normal = -normal;
+  Float n_dot_out = fabs(Dot(normal, out_dir));
+  in_dir = Normalize(-out_dir + 2.0 * Vector3f(normal) * n_dot_out);
+  return k_refl_ * c_refl_ * (1.0 / Dot(normal, in_dir));
+  /*
+  Float n_dot_out = fabs(Dot(hit_rec.n_hit_, out_dir));
+  in_dir = Normalize(-out_dir + 2.0 * Vector3f(hit_rec.n_hit_) * n_dot_out);
   return k_refl_ * c_refl_ * (1.0 /Dot(hit_rec.n_hit_, in_dir));
+  */
 }
 
 } // namespace leptus
