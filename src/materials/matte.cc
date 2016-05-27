51c886504eeff2fece4fafa39851567c1cfdaf2e
KukaTails <heweidong526@gmail.com>
2016-05-27 23:47:21 +0800

    set data private

--- src/materials/matte.cc
+++ src/materials/matte.cc
@@ -1,7 +1,7 @@
 #include "materials/matte.h"
 
+#include "core/world.h"
 #include "geometry/geometry.h"
-#include "world/world.h"
 
 namespace leptus {
 
@@ -16,19 +16,19 @@ Matte::Matte(const Color& color, Float k_ambient, Float k_diffuse)
 Color Matte::Shade(const ShadeRecord& shade_rec) const
 {
   const HitRecord& hit_rec = shade_rec.hit_rec_;
-  const std::vector<LightPtr>& lights = shade_rec.world_.lights_;
+  const std::vector<LightPtr>& lights = shade_rec.world_.lights();
 
   Vector3f out_dir = -shade_rec.ray_.dir_;
-  Color out_color = ambient_func_.Rho(shade_rec.hit_rec_, out_dir) * shade_rec.world_.ambient_light_->light();
+  Color out_color = ambient_func_.Rho(shade_rec.hit_rec_, out_dir) * shade_rec.world_.ambient_light()->light();
 
   for (int i = 0; i < lights.size(); ++i) {
     Vector3f in_dir = lights[i]->GetShadowRayDir(shade_rec.hit_rec_);
     Float n_dot_in = Dot(hit_rec.n_hit_ , in_dir);
     Float n_dot_out = Dot(hit_rec.n_hit_, out_dir);
 
-    if (lights[i]->CastShadows() && !lights[i]->InShadow(hit_rec.p_hit_, shade_rec.world_.objects_)) {
+    if (lights[i]->CastShadows() && !lights[i]->InShadow(hit_rec.p_hit_, shade_rec.world_.surfaces())) {
       if (n_dot_in > static_cast<Float>(0.0) && n_dot_out > static_cast<Float>(0.0))
-        out_color += diffuse_func_.F(hit_rec, in_dir, out_dir) * shade_rec.world_.lights_[i]->light() * n_dot_in;
+        out_color += diffuse_func_.F(hit_rec, in_dir, out_dir) * shade_rec.world_.lights()[i]->light() * n_dot_in;
     }
   }
   return out_color;
