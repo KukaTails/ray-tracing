47cb262f2c42f3a21a110a27ebdb78429124f60d
KukaTails <heweidong526@gmail.com>
2016-07-12 14:24:55 +0800

    add spherical mapping

---
+++ src/textures/spherical_map.h
@@ -0,0 +1,15 @@
+#ifndef LEPTUS_TEXTURE_SPHERICAL_MAP_H
+#define LEPTUS_TEXTURE_SPHERICAL_MAP_H
+#include "textures/mapping.h"
+
+namespace leptus {
+
+class SphericalMapping : public Mapping {
+public:
+  virtual 
+};
+
+
+} // namespace leptus
+
+#endif //LEPTUS_TEXTURE_SPHERICAL_MAP_H 
