363e22e3a0d09ce26008cc76524fad932dffa2e6
KukaTails <heweidong526@gmail.com>
2016-07-12 14:33:17 +0800

    add interfaces to get bounding box

--- src/core/surface.cc
+++ src/core/surface.cc
@@ -7,12 +7,55 @@ namespace leptus {
 
 const Float Surface::EPSILON = 1e-4;
 
+
+
 Surface::Surface(const Color& color)
-  :material_(std::make_shared<Matte>(color))
+  :material_(std::make_shared<Matte>(color)), sampler_(), bounding_box_()
 {}
 
 Surface::Surface(const MaterialPtr& material)
-  : material_(material)
+  : material_(material), sampler_(), bounding_box_()
 {}
+
+BoundingBoxPtr Surface::GetBoundingBox( ) const
+{
+  return std::make_shared<BoundingBox>(Point3f( ), Point3f( ));
+}
+
+bool Surface::BoundingBoxHit(const Ray& ray) const
+{
+  if (!bounding_box_)
+    bounding_box_ = GetBoundingBox( );
+
+  if (!bounding_box_)
+    return true;
+  else
+    return bounding_box_->Hit(ray);
+}
+
+MaterialPtr Surface::material( ) const
+{
+  return material_;
+}
+
+void Surface::SetSampler(const SamplerPtr& sampler)
+{
+  sampler_ = sampler;
+}
+
+Point3f Surface::Sample( ) const
+{
+  return Point3f( );
+}
+
+Float Surface::ProbabilityDensityFunc(const HitRecord& hit_rec) const
+{
+  return 0.0;
+}
+
+Normal3f Surface::GetNormal(const Point3f& point) const
+{
+  return Normal3f( );
+}
 
 } // namespace leptus
--- src/core/surface.h
+++ src/core/surface.h
@@ -2,11 +2,13 @@
 #define LEPTUS_CORE_SURFACE_H
 #include <memory>
 
+#include "accelerate/bounding_box.h"
 #include "core/color.h"
 #include "core/hitrecord.h"
 #include "core/leptus.h"
 #include "core/material.h"
 #include "core/ray.h"
+#include "core/sampler.h"
 
 namespace leptus {
 
@@ -17,8 +19,12 @@ public:
 
 protected:
   mutable MaterialPtr material_;
+  SamplerPtr sampler_;
+  mutable BoundingBoxPtr bounding_box_;
 
-private:
+protected:
+  bool BoundingBoxHit(const Ray& ray) const;
+  virtual BoundingBoxPtr GetBoundingBox( ) const;
   virtual Point3f GetLocalHitPoint(const Point3f& p_hit) const = 0;
 
 public:
@@ -26,9 +32,14 @@ public:
   Surface(const MaterialPtr& material);
   virtual ~Surface() = default;
 
-  MaterialPtr material() const { return material_; }
+  MaterialPtr material( ) const;
+  void SetSampler(const SamplerPtr& sampler);
   virtual bool ShadowHit(const Ray& ray, Float& t_hit) const = 0;
   virtual bool Hit(const Ray& ray, Float& t_hit, HitRecord& hit_rec) const = 0;
+  
+  virtual Point3f Sample( ) const;
+  virtual Normal3f GetNormal(const Point3f& point) const;
+  virtual Float ProbabilityDensityFunc(const HitRecord& hit_rec) const;
 };
 
 typedef Surface::SurPtr SurPtr;
