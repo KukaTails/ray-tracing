70f58ac989c5e3580494c68200eb467323ef790b
KukaTails <heweidong526@gmail.com>
2016-08-04 02:59:15 +0800

    add constructor

--- src/core/light.h
+++ src/core/light.h
@@ -6,6 +6,7 @@
 #include "core/color.h"
 #include "core/surface.h"
 #include "core/hitrecord.h"
+#include "core/shaderecord.h"
 #include "geometry/geometry.h"
 
 namespace leptus {
@@ -13,16 +14,22 @@ namespace leptus {
 class Light {
 public:
   typedef std::shared_ptr<Light> LightPtr;
-protected:
-  bool shadows_;
+
 public:
+  Light( ) : cast_shadows_(true) { }
+  explicit Light(bool cast_shadows) : cast_shadows_(cast_shadows) { }
   virtual ~Light() = default;
+  virtual Color light(const ShadeRecord& shader_rec) const = 0;
   virtual Vector3f GetShadowRayDir(const HitRecord& hit_rec) const = 0;
-  virtual Color light(const HitRecord& hit_rec) const = 0;
   virtual bool InShadow(const Point3f& point, const std::vector<SurPtr>& objects) const = 0;
-  virtual bool CastShadows() const = 0;
-  virtual Float GeometricFactor(const HitRecord& hit_rec) const { return 0.0; }
-  virtual Float ProbabilityDensityFunction(const HitRecord& hit_rec) const { return 0.0; }
+  virtual Float GeometricFactor(const HitRecord& hit_rec) const { return 1.0; }
+  virtual Float ProbabilityDensityFunction(const HitRecord& hit_rec) const { return 1.0; }
+  
+  bool CastShadows( ) const { return cast_shadows_; }
+  void SetCastShadows(bool setting) { cast_shadows_ = setting; }
+
+protected:
+  bool cast_shadows_;
 };
 
 typedef Light::LightPtr LightPtr;
